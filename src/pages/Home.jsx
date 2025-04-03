725f084553bf7cb0b492a6c6a9a79a0de86be6af
dev-joao-sousa <devsousamendes@gmail.com>
2025-04-03 15:50:33 -0300

    pages and components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Button from "../components/Button";
+import Cards from "../components/Card";
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <div>
+     <Header/> 
+     <Button/>
+     <Cards/>
+    </div>
+  );
+};
+
+export default Home;
